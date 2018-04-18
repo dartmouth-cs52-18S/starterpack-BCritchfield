bb0b4692118f2d25c7c31c4ecd3af0d937b0a215
Brittany Critchfield <brittany.critchfield.20@dartmouth.edu>
2018-04-18 03:16:44 -0400

    first

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('starting up!');
+// change require to es6 import style
+// import $ from 'jquery';
+
+import './styles.scss';
+
+// initialize variables and get first h1 tag which is time stamp
+const h1 = document.getElementsByTagName('h1')[0];
+
+let seconds = 0;
+
+
+function add() {
+  seconds += 1;
+  h1.textContent = `you've been on this website for this many seconds: ${
+    seconds > 9 ? seconds : `0${seconds}`}`;
+  setTimeout(add, 1000);
+}
+
+function timer() {
+  setTimeout(add, 1000);
+}
+timer();
